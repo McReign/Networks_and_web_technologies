2093caeea46f46bbc082079f60ea16052fc6f94f
McReign <ya.manshin@mail.ru>
2017-10-10 00:38:24 +0300

    adding TCPServer

---
+++ TCPServer/TCPServer/FirstServer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace TCPServer
+{
+    public class FirstServer
+    {
+        public static void StartServer() { 
+            TcpListener server = null;
+            TcpClient client = null;
+            try
+            {
+                IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddress, 5001);
+
+                server.Start();
+
+                Console.WriteLine("Ожидание подключений 5001... ");
+
+                client = server.AcceptTcpClient();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                Console.WriteLine("Подключен клиент.");
+
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    stream.Write(msg, 0, msg.Length);
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+                client.Close();
+            }
+        }
+    }
+}
---
+++ TCPServer/TCPServer/SecondServer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    public class SecondServer
+    {
+        public void StartServer()
+        {
+            TcpListener server = null;
+            TcpClient client = null;
+            try
+            {
+                IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddress, 5002);
+
+                server.Start();
+
+                Console.WriteLine("Ожидание подключений 5002... ");
+
+                client = server.AcceptTcpClient();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                Console.WriteLine("Подключен клиент.");
+
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    stream.Write(msg, 0, msg.Length);
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+                client.Close();
+            }
+        }
+    }
+}
